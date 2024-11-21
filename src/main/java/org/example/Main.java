5f3f3ccf742fc03d6a646dda18fcd43ee36a44b8
Quy Nguyen <qnguyen8084@sdsu.edu>
2024-11-21 04:29:19 -0800

    Add antlr4 to pom.xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello, World!");
+        String fileName = "turtleProgramFile.txt";
     }
 }
