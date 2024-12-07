1584cb49ba309efba60175f20705dacb5746d8dd
Quy Nguyen <qnguyen8084@sdsu.edu>
2024-12-07 15:28:31 -0800

    Add print statements for debugging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,12 @@ import org.example.generated.TurtleParser;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String fileName = "src/main/java/org/example/turtleProgramFile.txt";
+        String fileName;
+        if (args.length == 0) {
+            fileName = "src/main/java/org/example/turtleProgramFile.txt";
+        } else {
+            fileName = args[0];
+        }
         TurtleLexer lexer = new TurtleLexer(CharStreams.fromFileName(fileName));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         TurtleParser parser = new TurtleParser(tokens);
@@ -20,20 +25,46 @@ public class Main {
         System.out.println("This is the contents of the AST formed by the parser");
         System.out.println(program.toStringTree(parser));
 
-        TurtleMementoVisitor interpreter = new TurtleMementoVisitor();
+        TurtleCommandVisitor commandVisitor = new TurtleCommandVisitor();
+        TurtleMementoVisitor mementoVisitor = new TurtleMementoVisitor();
         TurtleDistanceVisitor distanceVisitor = new TurtleDistanceVisitor();
 
-        interpreter.visit(program);
+        commandVisitor.visit(program);
+        mementoVisitor.visit(program);
         distanceVisitor.visit(program);
 
+        System.out.println("This is the commands that the turtle executed");
+        List<String> commands = commandVisitor.getCommands();
+        for (String command : commands) {
+            System.out.println(command);
+        }
+
         System.out.println("This is the mementos of the turtle's state after each command");
-        List<TurtleMemento> mementos = interpreter.getMementos();
+        List<TurtleMemento> mementos = mementoVisitor.getMementos();
         for (TurtleMemento memento : mementos) {
             System.out.println("Memento - ("+ memento.x() + "," + memento.y() + "," + memento.heading() + ")");
         }
 
+        System.out.println("This is the composite list of commands and mementos:");
+        for (int i = 0; i < commands.size(); i++) {
+            String command = commands.get(i);
+            TurtleMemento memento = mementos.get(i);
+            System.out.println("Command: " + command + " | Memento - (" + memento.x() + "," + memento.y() + "," + memento.heading() + ")");
+        }
+
+        List<Double> distances = distanceVisitor.getTotalDistances();
+
+        System.out.println("This is the composite list of commands, mementos, and distances:");
+        for (int i = 0; i < commands.size(); i++) {
+            String command = commands.get(i);
+            TurtleMemento memento = mementos.get(i);
+            double distance = distances.get(i);
+            System.out.println("Command: " + command + " | Memento - (" + memento.x() + "," + memento.y() + "," +
+                    memento.heading() + ") | Distance: " + distance);
+        }
+
         System.out.println("This is the total and net distance traveled by the turtle");
         System.out.println("Total distance traveled: " + distanceVisitor.getTotalDistance());
-        System.out.println("Net distance traveled: " + interpreter.getNetDistance());
+        System.out.println("Net distance traveled: " + mementoVisitor.getNetDistance());
     }
 }
--- src/main/java/org/example/TurtleDistanceVisitor.java
+++ src/main/java/org/example/TurtleDistanceVisitor.java
@@ -3,19 +3,28 @@ package org.example;
 import org.example.generated.TurtleBaseVisitor;
 import org.example.generated.TurtleParser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TurtleDistanceVisitor extends TurtleBaseVisitor<Void> {
     private double totalDistance = 0;
     private int x = 0;
     private int y = 0;
+    private final List<Double> totalDistances = new ArrayList<>();
 
     public double getTotalDistance() {
         return totalDistance;
+    }
+
+    public List<Double> getTotalDistances() {
+        return new ArrayList<>(totalDistances);
     }
 
     @Override
     public Void visitForward(TurtleParser.ForwardContext ctx) {
         int distance = Integer.parseInt(ctx.INT().getText());
         totalDistance += distance;
+        totalDistances.add(totalDistance);
         return null;
     }
 
@@ -23,6 +32,7 @@ public class TurtleDistanceVisitor extends TurtleBaseVisitor<Void> {
     public Void visitBackward(TurtleParser.BackwardContext ctx) {
         int distance = Integer.parseInt(ctx.INT().getText());
         totalDistance += distance;
+        totalDistances.add(totalDistance);
         return null;
     }
 
@@ -33,6 +43,27 @@ public class TurtleDistanceVisitor extends TurtleBaseVisitor<Void> {
         totalDistance += Math.hypot(newX - x, newY - y);
         x = newX;
         y = newY;
+        totalDistances.add(totalDistance);
+        return null;
+    }
+
+    public Void visitSetHeading(TurtleParser.SetHeadingContext ctx) {
+        totalDistances.add(totalDistance);
+        return null;
+    }
+
+    public Void visitLeft(TurtleParser.LeftContext ctx) {
+        totalDistances.add(totalDistance);
+        return null;
+    }
+
+    public Void visitRight(TurtleParser.RightContext ctx) {
+        totalDistances.add(totalDistance);
+        return null;
+    }
+
+    public Void visitTurtle(TurtleParser.TurtleContext ctx) {
+        totalDistances.add(totalDistance);
         return null;
     }
 }
